c729f5d403455f6c31a9ab6fbf984fa03d21ce89
horennel <nelsonhoren@gmail.com>
2024-03-04 19:55:32 +0800

    update latex_ocr code

---
+++ latex_ocr.py
@@ -0,0 +1,34 @@
+from pix2tex.cli import LatexOCR
+from PIL import ImageGrab
+import pyperclip
+import rumps
+
+model = LatexOCR()
+
+
+class LatexOrc(rumps.App):
+    def __init__(self, name):
+        super(LatexOrc, self).__init__(name=name, icon='./icons/menubar_ogo.png', quit_button="Quit")
+
+    @rumps.clicked("Start OCR")
+    def prefs(self, _):
+        image = ImageGrab.grabclipboard()
+        latex_string = model(image)
+        pyperclip.copy(latex_string)
+        rumps.notification("Success!", latex_string, latex_string)
+
+    @rumps.notifications
+    def notification_center(self, info):
+        pass
+
+    @rumps.clicked("On / Off")
+    def onoff(self, _):
+        print_button = self.menu['Start OCR']
+        if print_button.callback is None:
+            print_button.set_callback(self.prefs)
+        else:
+            print_button.set_callback(None)
+
+
+if __name__ == "__main__":
+    LatexOrc(name='').run()
